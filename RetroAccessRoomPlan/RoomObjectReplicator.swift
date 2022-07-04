d770bf6a35c85a29fca3e85a1116b537bcd396f4
kaiming-uw <kaimingc@cs.washington.edu>
2022-07-04 12:22:22 -0700

    Demo code for retroAccess: RoomPlan + dimension conversion

---
+++ RetroAccessRoomPlan/RoomObjectReplicator.swift
@@ -0,0 +1,94 @@
+import ARKit
+import RoomPlan
+
+public class RoomObjectAnchor: ARAnchor {
+
+    public override var identifier: UUID {
+        roomObjectIdentifier
+    }
+
+    public override var transform: simd_float4x4 {
+        roomObjectTransform
+    }
+
+    public private(set) var dimensions: simd_float3
+    public private(set) var category: CapturedRoom.Object.Category
+
+    private let roomObjectIdentifier: UUID
+    private var roomObjectTransform: simd_float4x4
+
+    public required init(anchor: ARAnchor) {
+        guard let anchor = anchor as? RoomObjectAnchor else {
+            fatalError("RoomObjectAnchor can only copy other RoomObjectAnchor instances")
+        }
+
+        roomObjectIdentifier = anchor.roomObjectIdentifier
+        roomObjectTransform = anchor.roomObjectTransform
+        dimensions = anchor.dimensions
+        category = anchor.category
+
+        super.init(anchor: anchor)
+    }
+
+    @available(*, unavailable)
+    public required init?(coder: NSCoder) {
+        fatalError("Unavailable")
+    }
+
+    public init(_ object: CapturedRoom.Object) {
+        roomObjectIdentifier = object.identifier
+        roomObjectTransform = object.transform
+        dimensions = object.dimensions
+        category = object.category
+        super.init(transform: object.transform)
+    }
+
+    fileprivate func update(_ object: CapturedRoom.Object) {
+        roomObjectTransform = object.transform
+        dimensions = object.dimensions
+        category = object.category
+    }
+
+}
+
+public class RoomObjectReplicator {
+
+    private var trackedAnchors: Set<RoomObjectAnchor>
+    private var trackedAnchorsByIdentifier: [UUID: RoomObjectAnchor]
+    private var inflightAnchors: Set<RoomObjectAnchor>
+
+    public init() {
+        trackedAnchors = Set<RoomObjectAnchor>()
+        trackedAnchorsByIdentifier = [UUID: RoomObjectAnchor]()
+        inflightAnchors = Set<RoomObjectAnchor>()
+    }
+
+    public func anchor(objects: [CapturedRoom.Object], in session: RoomCaptureSession) {
+        for object in objects {
+            if let existingAnchor = trackedAnchorsByIdentifier[object.identifier] {
+                existingAnchor.update(object)
+                inflightAnchors.insert(existingAnchor)
+                session.arSession.delegate?.session?(session.arSession, didUpdate: [existingAnchor])
+            } else {
+                let anchor = RoomObjectAnchor(object)
+                inflightAnchors.insert(anchor)
+                session.arSession.add(anchor: anchor)
+            }
+        }
+
+        trackInflightAnchors(in: session)
+    }
+
+    private func trackInflightAnchors(in session: RoomCaptureSession) {
+        trackedAnchors.subtracting(inflightAnchors).forEach(session.arSession.remove)
+        trackedAnchors.removeAll(keepingCapacity: true)
+        trackedAnchors.formUnion(inflightAnchors)
+        inflightAnchors.removeAll(keepingCapacity: true)
+        trackedAnchorsByIdentifier.removeAll(keepingCapacity: true)
+
+        for trackedAnchor in trackedAnchors {
+            trackedAnchorsByIdentifier[trackedAnchor.identifier] = trackedAnchor
+        }
+    }
+
+}
--- RetroAccessRoomPlan/ViewController.swift
+++ RetroAccessRoomPlan/ViewController.swift
@@ -5,20 +5,64 @@
 //  Created by Kaiming on 7/4/22.
 //
 
+import ARKit
 import UIKit
 import RealityKit
+import RoomPlan
 
 class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
     
+    var captureSession: RoomCaptureSession?
+    var replicator = RoomObjectReplicator()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        captureSession = RoomCaptureSession()
+        captureSession?.delegate = self
+        captureSession?.run(configuration: .init())
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
     }
+
+}
+
+extension ViewController: RoomCaptureSessionDelegate {
+
+    func captureSession(_ session: RoomCaptureSession, didAdd room: CapturedRoom) {
+        replicator.anchor(objects: room.objects, in: session)
+    }
+
+    func captureSession(_ session: RoomCaptureSession, didChange room: CapturedRoom) {
+        replicator.anchor(objects: room.objects, in: session)
+    }
+
+    func captureSession(_ session: RoomCaptureSession, didUpdate room: CapturedRoom) {
+        replicator.anchor(objects: room.objects, in: session)
+    }
+
+    func captureSession(_ session: RoomCaptureSession, didRemove room: CapturedRoom) {
+        replicator.anchor(objects: room.objects, in: session)
+    }
+
+    func captureSession(_ session: RoomCaptureSession, didStartWith configuration: RoomCaptureSession.Configuration) {
+        arView.session.pause()
+        arView.session = session.arSession
+        arView.session.delegate = self
+    }
+
+}
+
+extension ViewController: ARSessionDelegate {
+
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        arView.scene.addRoomObjectEntities(for: anchors)
+    }
+
+    func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+        arView.scene.updateRoomObjectEntities(for: anchors)
+    }
+
 }
